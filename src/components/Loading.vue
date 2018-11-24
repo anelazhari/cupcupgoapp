ded3a12f401a679aae625229adc04f6f3bc1fbce
João Corsini <joao.corsini@f-secure.com>
2018-11-24 23:01:55 +0200

    Create new loading page

---
+++ src/components/Loading.vue
@@ -0,0 +1,26 @@
+<template>
+  <img src="../assets/loading.gif">
+</template>
+
+<script>
+export default {
+  name: "Loading",
+  props: {
+    msg: String
+  },
+  components: {},
+  methods: {
+    toWelcome: function () {
+      this.$router.push('/Welcome');
+    },
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
